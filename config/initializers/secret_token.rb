34ca8728d6d4e4cb85dbf5a5ed0cc9131e86a229
Michael Elfassy <michaelelfassy@napps.ca>
2012-11-30 18:28:55 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitoogle::Application.config.secret_token = 'fec8f27c172fa86d22239d10107bd804b1bdfe956731269bfbeaf85cf0de52746f1084b77d8f740fffce3a145f1031ad07c4b77522cf003404b114a8212149d5'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Twitoogle::Application.routes.draw do
+  root :to => "home#index"
+end
